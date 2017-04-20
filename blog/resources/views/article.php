02bda5b5c488739f4e3bd845ca44bb6667336c30
nabil el moudden <nabilelmoudden@gmail.com>
2017-04-20 09:45:02 +0000

    test
    test

---
+++ blog/resources/views/article.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Les articles</title>
+</head>
+<body>
+téléchargé depuis  dcercferc zedk,"zeknzklencklzneckzneclkznelkcnzeklcn
+www.fahamni.tn 2016/2017 12
+<p>C'est l'article n° <?php echo $numero ?></p>
+</body>
+</html>
